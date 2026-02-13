87d4f3d550e3d698f979c518b87c87c8a42cc677
Tamkhan4552 <hizro123@gmail.com>
2026-02-13 11:42:16 +0500

    communityinteraction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-background/80 border-b border-border">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          {/* Logo */}
+          {/* Logo12 */}
           <a 
             href="https://www.cosmbreath.com/"
             className="flex items-center gap-2 cursor-pointer"
